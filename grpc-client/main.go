3bcebc7634c98f99c45190910a9712ca285c5ebe
ayush-pandya <ayushpandya517@gmail.com>
2025-06-15 13:05:03 +0530

    fixing the docker

--- grpc-client/main.go
+++ grpc-client/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	// Connect to gRPC server
-	conn, err := grpc.NewClient("localhost:50051",
+	conn, err := grpc.NewClient("grpc-server:50051",
 		grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
